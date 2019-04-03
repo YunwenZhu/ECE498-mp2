0c5e186bb306a967468086003d233066566a9a59
AvaZhu <avazhu97@gmail.com>
2019-04-03 11:03:24 -0500

    gg

--- java/com/practice/ece498mp1/MainActivity.java
+++ java/com/practice/ece498mp1/MainActivity.java
@@ -5,7 +5,6 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,7 +13,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Timestamp;
@@ -28,25 +26,22 @@ import org.apache.commons.csv.*;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private static final String TAG = "MainActivity";
     private SensorManager sensorManager;
-    private Sensor accelerometer, gyroscope, magneticField, lightSensor;
-    private double Accel_x, Accel_y, Accel_z, Gyro_x, Gyro_y, Gyro_z, Mag_x, Mag_y, Mag_z, lightIntensity;
+    private Sensor accelerometer, sound, wifi;
+    private double Accel_x, Accel_y, Accel_z;
     private Timestamp timestamp;
     private HashSet<String> s = new HashSet<>();
     private CSVPrinter printer;
     private boolean recording_;
     private String outputFile;
+    private boolean firstTime = true;
 
-    float [] mag = new float[3];
-    float [] acc = new float[3];
-    Boolean firstMag = false;
-    Boolean firstAccel = false;
-    Boolean firstData = false;
-    float degreesTurned;
-    float previousVel, prevOrientation;
+    private double ema = 10.45; //hard code
+    private double prev = 0.0;
+    private int step = 0;
+    private double dis = 0.0;
+    private double dis_per_step = 0.35;
 
-
-
-    TextView xValue, yValue, zValue, step_count, distance, turned;
+    TextView xValue, yValue, zValue, step_count, distance;
 
 
 
@@ -60,7 +55,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         zValue = (TextView) findViewById(R.id.zValue);
         step_count = (TextView) findViewById(R.id.step_count);
         distance = (TextView) findViewById(R.id.distance);
-        turned = (TextView) findViewById(R.id.turned);
 
         Log.d(TAG, "onCreate: Initializing Sensor Services");
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -69,23 +63,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         Log.d(TAG, "onCreate: Registered accelerometer listener");
 
-        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
-        Log.d(TAG, "onCreate: Registered gyroscope listener");
-//
-        magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        sensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_NORMAL);
-        Log.d(TAG, "onCreate: Registered magneticField listener");
-//
-//        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-//        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-//        Log.d(TAG, "onCreate: Registered lightSensor listener");
-
         Timestamp startTime;
 
         recording_ = false;
-
-
     }
 
 
@@ -98,11 +78,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         if (recording_) {
             Sensor sensor = event.sensor;
-            float [] orientationValues = new float[3];
 
             if (sensor.getType() == Sensor.TYPE_ACCELEROMETER && !s.contains("Accel")) {
-                acc = event.values;
-                firstAccel = true;
 
                 Accel_x = event.values[0];
                 Accel_y = event.values[1];
@@ -113,75 +90,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 zValue.setText("Accel_z:" + Accel_z);
                 Log.d(TAG, "acccccce");
 
-//            } else if (sensor.getType() == Sensor.TYPE_GYROSCOPE && !s.contains("Gyro")) {
-//                Gyro_x = event.values[0];
-//                Gyro_y = event.values[1];
-//                Gyro_z = event.values[2];
-
-            } else if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD && !s.contains("Mag")) {
-                mag = event.values;
-                firstMag = true;
-                Log.d(TAG, "#######################");
-            }
-
-            else if (sensor.getType() == Sensor.TYPE_GYROSCOPE){
-                if (!firstData && firstMag && firstAccel){
-                    Log.e("working??", "bruhtf");
-
-                    Log.e("wein", "______________________________");
-
-                    previousVel = Math.abs(event.values[2]);
-                    findOrientation(orientationValues);
-                    prevOrientation = orientationValues[0];
-                    firstData = true;
+                if(firstTime){
+                    prev = Math.sqrt(Math.pow(Accel_x, 2) + Math.pow(Accel_y, 2) + Math.pow(Accel_z, 2));
+                    firstTime = false;
                 }
-                else if (firstData){
-                    Log.e("working??", "START");
-
-                    float z = Math.abs(event.values[2]);
-
-                    findOrientation(orientationValues);
-
-                    if ((z - previousVel) > 0.005 && orientationValues != null) {
-                        float orientation = orientationValues[0];
-                        if (event.values[2] > 0) {
-                            //turned Left (relatively)
-                            if ((orientation > 0 && prevOrientation < 0)) {
-                                degreesTurned += ((Math.PI + prevOrientation) + (Math.PI - orientation)) * 180/Math.PI;
-                            } else if (prevOrientation > 0 && orientation < 0) {
-                                degreesTurned += ((prevOrientation) + (0 - orientation)) * 180 / Math.PI;
-                            } else {
-                                degreesTurned += (Math.abs(orientation - prevOrientation)) * 180/Math.PI;
-                            }
+                else {
+                    double cur = Math.sqrt(Math.pow(Accel_x, 2) + Math.pow(Accel_y, 2) + Math.pow(Accel_z, 2));
+                    if(cur <= ema && prev >= ema){
+                        if(Math.abs(cur - ema) >= 0.1){
+                            step += 1;
                         }
-                        else {
-                            //turned right (relatively)
-                            if ((orientation > 0 && prevOrientation < 0)) {
-                                degreesTurned += ((0 - prevOrientation) + orientation) * 180/Math.PI;
-                            } else if (prevOrientation > 0 && orientation < 0) {
-                                degreesTurned += ((Math.PI + orientation) + (Math.PI - prevOrientation)) * 180/Math.PI;
-                            } else {
-                                degreesTurned += (Math.abs(orientation - prevOrientation)) * 180/Math.PI;
-                            }
-
-                        }
-//                            degreesTurned += (dt * ((z + previousVel)/2))*180/Math.PI;
-                        turned = (TextView) findViewById(R.id.turned);
-                        turned.setText("degreesTurned:" + degreesTurned);
-
                     }
-                    previousVel = z;
-
-
-
+                    dis = dis_per_step * step;
+                    step_count.setText("Step: " + step);
+                    distance.setText("Distance:" + dis + "m");
+                    prev = cur;
+                }
 
             }
-            }
-
-//            } else if (sensor.getType() == Sensor.TYPE_LIGHT && !s.contains("Light")) {
-//                lightIntensity = event.values[0];
-
-//            }
 
             timestamp = new Timestamp(System.currentTimeMillis());
             Log.i(TAG, "log to file");
@@ -190,21 +116,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-
-
         }
     }
 
-    protected Boolean findOrientation(float [] values){
-        float [] R = new float[9];
-        float [] I = new float[9];
-        if (acc != null && mag != null) {
-            SensorManager.getRotationMatrix(R, I , acc, mag);
-            SensorManager.getOrientation(R, values);
-            return true;
-        }
-        return false;
-    }
 
     public void toggleRecording(View view) throws IOException {
         Button b = (Button) view;
@@ -217,29 +131,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
             try {
                 DateFormat df = new SimpleDateFormat("YYYY-MM-DD-HH:mm:ss");
-                File folder = new File(Environment.getExternalStorageDirectory() + "/MP2/");
-                boolean isPresent = true;
-                if (!folder.exists()) {
-                    isPresent = folder.mkdir();
-                }
-                if (isPresent) {
-
-                    outputFile = Environment.getExternalStorageDirectory() + "/ECE498 mp2/" + t.getText().toString() + "-" + df.format(new Date()) + ".csv";
-                    printer = new CSVPrinter(new FileWriter(outputFile), CSVFormat.DEFAULT.withHeader("Time Stamp", "Accel_x", "Accel_y", "Accel_z"));
-                } else {
-                    Log.e("stop", "folder not made");
-                }
-//                outputFile = Environment.getExternalStorageDirectory() + "/ECE498 mp2/" + t.getText().toString() + "-" + df.format(new Date()) + ".csv";
-//                printer = new CSVPrinter(new FileWriter(outputFile), CSVFormat.DEFAULT.withHeader("Time Stamp", "Accel_x", "Accel_y", "Accel_z"));
+                outputFile = getExternalFilesDir(null) + t.getText().toString() + "-" + df.format(new Date()) + ".csv";
+                printer = new CSVPrinter(new FileWriter(outputFile), CSVFormat.DEFAULT.withHeader("Time Stamp", "Accel_x", "Accel_y", "Accel_z"));
             } catch (IOException ex) {
                 Log.e(TAG, ex.toString());
                 throw ex;
             }
+            step = 0;
+            dis = 0.0;
             b.setText("Stop");
-            degreesTurned = 0;
             t.setEnabled(false);
             Log.i(TAG, "Start recording");
         } else {
+            step_count.setText("Step: " + 0);
+            distance.setText("Distance:" + 0.0 + "m");
             printer.close(true);
             b.setText("Start");
             t.setEnabled(true);
